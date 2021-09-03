f6dc4682237e5a91cc3bb9cf687952eae927940a
Stephen <33627847+StephenDevenney@users.noreply.github.com>
2021-09-03 14:50:33 +0100

    Initial Commit

---
+++ PizzaBackend/Pizza.Express.Shared/Security/AppSettings.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Pizza.Express.Shared.Security
+{
+    public class AppSettings
+    {
+        public string PortalUrl { get; set; }
+        public string PortalUrlCors { get; set; }
+        public string Environment { get; set; }
+        public string Secret { get; set; }
+    }
+}
---
+++ PizzaBackend/Pizza.Express.Shared/Security/SecureRole.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Pizza.Express.Shared.Security
+{
+    public static class SecureRole
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
