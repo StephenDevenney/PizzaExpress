9d61cfb8b14879cc1942fad4d13e93715e1bf3df
Stephen <33627847+StephenDevenney@users.noreply.github.com>
2021-09-03 16:24:28 +0100

    Auth and API startup file configuration.

--- PizzaBackend/Pizza.Express.Auth/Startup.cs
+++ PizzaBackend/Pizza.Express.Auth/Startup.cs
@@ -1,56 +1,109 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Pizza.Express.Shared.Security;
+using Pizza.Express.Context;
+using Pizza.Express.Handler.Interfaces;
+using Pizza.Express.Context.Interfaces;
+using Pizza.Express.Handler;
 
 namespace Pizza.Express.Auth
 {
     public class Startup
     {
+        #region CONSTRUCTOR
+        private IConfiguration Configuration { get; }
+        private readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
+        #endregion
 
-        public IConfiguration Configuration { get; }
-
+        #region CONFIGURESERVICES
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            #region JWT
+            // JWT Configuration
+            var appSettingsSection = Configuration.GetSection("App");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            // Authentication
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+            #endregion
+
+            #region USERROLES
+            // Authorisation
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Admin", policy => policy.RequireRole("Admin"));
+                options.AddPolicy("User", policy => policy.RequireRole("User"));
+            });
+            #endregion
+
+            #region EFCORE
+            // EfCore
+            services.AddDbContextPool<SqlContext>(options =>
+            {
+                options.UseSqlite(Configuration.GetConnectionString("AppDB"));
+            });
+            #endregion
+
+            #region SERVICES
+            services.AddScoped<IAuthHandler, AuthHandler>();
+            services.AddScoped<IAuthContext, AuthContext>();
+            #endregion
+
+            #region CORS
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                        builder => builder.WithOrigins(appSettings.PortalUrlCors.Split(","))
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+            });
+            #endregion
+
+            services.AddControllers();
         }
+        #endregion
 
+        #region CONFIGURE
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
             app.UseRouting();
-
+            app.UseCors(MyAllowSpecificOrigins);
+            app.UseAuthentication();
             app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
             });
         }
+        #endregion
     }
 }
