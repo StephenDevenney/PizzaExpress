84de4fb2c9d1b60a40112b1144659b14f8e961b2
Stephen <33627847+StephenDevenney@users.noreply.github.com>
2021-09-04 14:29:16 +0100

    pizza tables

---
+++ PizzaBackend/Pizza.Express.Database/Tables/Pizza/Order.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [pizza].[Order]
+(
+	[OrderId] INT IDENTITY(1,1) PRIMARY KEY, 
+    [FK_ClientId] INT NOT NULL, 
+    [FK_StatusId] INT NOT NULL,
+    [OrderCode] NVARCHAR(50) NOT NULL,
+
+    CONSTRAINT [Order_Client_ClientId] FOREIGN KEY ([FK_ClientId]) REFERENCES [security].[Client] (ClientId),
+    CONSTRAINT [Order_Status_StatusId] FOREIGN KEY ([FK_StatusId]) REFERENCES [enum.pizza].[Status] (StatusId)
+)
---
+++ PizzaBackend/Pizza.Express.Database/Tables/Pizza/OrderedPizza.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [pizza].[OrderedPizza]
+(
+	[OrderedPizzaId] INT IDENTITY(1,1) PRIMARY KEY, 
+    [FK_OrderId] INT NOT NULL, 
+    [FK_PizzaId] INT NOT NULL, 
+
+    CONSTRAINT [OrderedPizza_Order_StatusId] FOREIGN KEY ([FK_OrderId]) REFERENCES [pizza].[Order] (OrderId),
+    --CONSTRAINT [OrderedPizza_Pizza_StatusId] FOREIGN KEY ([FK_PizzaId]) REFERENCES [pizza].[Status] (StatusId)
+)
