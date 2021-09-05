d8418cf8cb4f722aafb1087babc2d1693a269423
Stephen <33627847+StephenDevenney@users.noreply.github.com>
2021-09-05 16:42:30 +0100

    nav menu service

---
+++ PizzaBackend/Pizza.Express.Handler/Interfaces/ISecurityHandler.cs
@@ -0,0 +1,21 @@
+﻿using Pizza.Express.Shared.ViewModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Pizza.Express.Handler.Interfaces
+{
+    public interface ISecurityHandler
+    {
+        #region GET
+        public Task<List<NavMenuViewModel>> GetNavMenu();
+        #endregion
+
+        #region PUT
+
+        #endregion
+
+        #region POST
+
+        #endregion
+    }
+}
---
+++ PizzaBackend/Pizza.Express.Handler/SecurityHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Pizza.Express.Context.Interfaces;
+using Pizza.Express.Handler.Interfaces;
+using Pizza.Express.Shared.ViewModels;
+
+namespace Pizza.Express.Handler
+{
+    public class SecurityHandler : ISecurityHandler
+    {
+        #region CONSTRUCTOR
+        private readonly ISecurityContext securityRepo;
+        public SecurityHandler(ISecurityContext securityRepo)
+        {
+            this.securityRepo = securityRepo;
+        }
+        #endregion
+
+        #region GET
+        public async Task<List<NavMenuViewModel>> GetNavMenu() => await securityRepo.GetNavMenu();
+        #endregion
+
+        #region PUT
+
+        #endregion
+
+        #region POST
+
+        #endregion
+
+    }
+}
