0afd5f5fbbfbddb5a60baf95d96d7799168db94e
Stephen Devenney <stephen_devenney@yahoo.com>
2021-09-06 00:13:37 +0100

    product handler

---
+++ PizzaBackend/Pizza.Express.Handler/Interfaces/IProductHandler.cs
@@ -0,0 +1,21 @@
+﻿using Pizza.Express.Shared.ViewModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Pizza.Express.Handler.Interfaces
+{
+    public interface IProductHandler
+    {
+        #region GET
+        public Task<List<PizzaViewModel>> GetPizza();
+        #endregion
+
+        #region PUT
+
+        #endregion
+
+        #region POST
+
+        #endregion
+    }
+}
---
+++ PizzaBackend/Pizza.Express.Handler/ProductHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Pizza.Express.Context.Interfaces;
+using Pizza.Express.Handler.Interfaces;
+using Pizza.Express.Shared.ViewModels;
+
+namespace Pizza.Express.Handler
+{
+    public class ProductHandler : IProductHandler
+    {
+        #region CONSTRUCTOR
+        private readonly IProductContext productRepo;
+        private readonly IGlobals globals;
+        public ProductHandler(IGlobals _globals, 
+                                IProductContext productRepo)
+        {
+            this.productRepo = productRepo;
+            this.globals = _globals;
+        }
+        #endregion
+
+        #region GET
+        public async Task<List<PizzaViewModel>> GetPizza() => await productRepo.GetPizza();
+        #endregion
+
+        #region PUT
+
+        #endregion
+
+        #region POST
+
+        #endregion
+
+    }
+}
