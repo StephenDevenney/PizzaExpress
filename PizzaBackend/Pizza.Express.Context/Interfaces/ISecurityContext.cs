547d42b0ceec9676ef698f7df58680cab7419fa4
Stephen <33627847+StephenDevenney@users.noreply.github.com>
2021-09-05 16:42:39 +0100

    nav menu context

---
+++ PizzaBackend/Pizza.Express.Context/Interfaces/ISecurityContext.cs
@@ -0,0 +1,21 @@
+﻿using Pizza.Express.Shared.ViewModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Pizza.Express.Context.Interfaces
+{
+    public interface ISecurityContext
+    {
+        #region GET
+        public Task<List<NavMenuViewModel>> GetNavMenu();
+        #endregion
+
+        #region PUT
+
+        #endregion
+
+        #region POST
+
+        #endregion
+    }
+}
---
+++ PizzaBackend/Pizza.Express.Context/SecurityContext.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Pizza.Express.Context.Interfaces;
+using Pizza.Express.Shared.Entities;
+using Pizza.Express.Shared.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Pizza.Express.Context
+{
+    public class SecurityContext: ISecurityContext
+    {
+        #region CONSTRUCTOR
+        private readonly SqlContext sqlContext;
+        private readonly IGlobals globals;
+        public SecurityContext(IGlobals _globals,
+                                SqlContext _sqlRepo)
+        {
+            this.sqlContext = _sqlRepo;
+            this.globals = _globals;
+        }
+        #endregion
+
+        #region GET
+        public async Task<List<NavMenuViewModel>> GetNavMenu()
+        {
+            UserEntity user = await this.globals.GetCurrentUser();
+            return await sqlContext.NavMenu.Join(sqlContext.NavMenuRole.Where(nmr => nmr.UserRoleId == user.UserRoleId),
+                nm => nm.NavMenuId,
+                nmr => nmr.NavMenuId,
+                (nm, nmr) => new NavMenuViewModel
+                {
+                    NavMenuName = nm.NavMenuName,
+                    NavMenuTitle = nm.NavMenuTitle,
+                    NavMenuRoute = nm.NavMenuRoute
+                }).ToListAsync();
+        }
+        #endregion
+
+        #region PUT
+        
+        #endregion
+
+        #region POST
+
+        #endregion
+    }
+}
