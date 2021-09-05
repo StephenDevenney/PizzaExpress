049d414aba34c4efce9f5697e0a525c79b6b1325
Stephen Devenney <stephen_devenney@yahoo.com>
2021-09-06 00:14:09 +0100

    product context get pizza

---
+++ PizzaBackend/Pizza.Express.Context/Interfaces/IProductContext.cs
@@ -0,0 +1,21 @@
+﻿using Pizza.Express.Shared.ViewModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Pizza.Express.Context.Interfaces
+{
+    public interface IProductContext
+    {
+        #region GET
+        public Task<List<PizzaViewModel>> GetPizza();
+        #endregion
+
+        #region PUT
+
+        #endregion
+
+        #region POST
+
+        #endregion
+    }
+}
---
+++ PizzaBackend/Pizza.Express.Context/ProductContext.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Pizza.Express.Context.Interfaces;
+using Pizza.Express.Shared.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Pizza.Express.Context
+{
+    public class ProductContext: IProductContext
+    {
+        #region CONSTRUCTOR
+        private readonly SqlContext sqlContext;
+        private readonly IGlobals globals;
+        public ProductContext(IGlobals _globals,
+                                SqlContext _sqlRepo)
+        {
+            this.sqlContext = _sqlRepo;
+            this.globals = _globals;
+        }
+        #endregion
+
+        #region GET
+        public async Task<List<PizzaViewModel>> GetPizza()
+        {
+            return await sqlContext.Pizza.Select(res => new PizzaViewModel { Name = res.Name, Description = res.Description, Price = res.Price, ImageLink = res.ImageLink }).ToListAsync();
+        }
+        #endregion
+
+        #region PUT
+        
+        #endregion
+
+        #region POST
+
+        #endregion
+    }
+}
