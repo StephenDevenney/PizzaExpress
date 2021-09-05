30f23c7f76e103929b4f9d26b236721cb9e92058
Stephen Devenney <stephen_devenney@yahoo.com>
2021-09-06 00:13:15 +0100

    pizza entity and viewmodel

---
+++ PizzaBackend/Pizza.Express.Shared/Entities/PizzaEntity.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Pizza.Express.Shared.Entities
+{
+    public class PizzaEntity
+    {
+        public int PizzaId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageLink { get; set; }
+    }
+}
---
+++ PizzaBackend/Pizza.Express.Shared/ViewModels/PizzaViewModel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Pizza.Express.Shared.ViewModels
+{
+    public class PizzaViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageLink { get; set; }
+    }
+}
