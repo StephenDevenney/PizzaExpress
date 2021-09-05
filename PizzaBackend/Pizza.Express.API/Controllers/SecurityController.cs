68d09b15457910f3482052ecc933a52af761fa99
Stephen <33627847+StephenDevenney@users.noreply.github.com>
2021-09-05 16:42:55 +0100

    nav menu controller and db context

---
+++ PizzaBackend/Pizza.Express.API/Controllers/SecurityController.cs
@@ -0,0 +1,36 @@
+﻿using Pizza.Express.Handler.Interfaces;
+using Pizza.Express.Shared.Security;
+using Pizza.Express.Shared.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Pizza.Express.API.Controllers
+{
+    [Route("Security")]
+    public class SecurityController : ControllerBase
+    {
+        #region CONSTRUCTOR
+        private readonly ISecurityHandler securityHandler;
+        public SecurityController(ISecurityHandler securityHandler)
+        {
+            this.securityHandler = securityHandler;
+        }
+        #endregion
+
+        #region GET
+        [Authorize(Roles = SecureRole.Admin + "," + SecureRole.User)]
+        [HttpGet("nav-menu")]
+        public async Task<List<NavMenuViewModel>> GetNavMenu() => await securityHandler.GetNavMenu();
+        #endregion
+
+        #region PUT 
+
+        #endregion
+
+        #region POST
+
+        #endregion
+    }
+}
--- PizzaBackend/Pizza.Express.Context/DbContext/SqlContext.cs
+++ PizzaBackend/Pizza.Express.Context/DbContext/SqlContext.cs
@@ -14,6 +14,8 @@ namespace Pizza.Express.Context
         #region DBSET
         public DbSet<UserEntity> User { get; set; }
         public DbSet<UserRoleEntity> UserRole { get; set; }
+        public DbSet<NavMenuEntity> NavMenu { get; set; }
+        public DbSet<NavMenuRoleEntity> NavMenuRole { get; set; }
         #endregion
 
         #region MODELBUILDER
@@ -21,6 +23,8 @@ namespace Pizza.Express.Context
         {
             modelBuilder.Entity<UserEntity>(DBUser);
             modelBuilder.Entity<UserRoleEntity>(DBUserRole);
+            modelBuilder.Entity<NavMenuEntity>(DBNavMenu);
+            modelBuilder.Entity<NavMenuRoleEntity>(DBNavMenuRole);
             base.OnModelCreating(modelBuilder);
         }
         #endregion
@@ -33,6 +37,15 @@ namespace Pizza.Express.Context
             _.Property<int>(x => x.UserRoleId).HasColumnName("UserRoleId");
             _.Property<string>(x => x.RoleName).HasColumnName("RoleName");
         }
+        private void DBNavMenu(EntityTypeBuilder<NavMenuEntity> _)
+        {
+            _.ToTable("NavMenu", "enum");
+            _.HasKey(x => x.NavMenuId);
+            _.Property<int>(x => x.NavMenuId).HasColumnName("NavMenuId");
+            _.Property<string>(x => x.NavMenuName).HasColumnName("NavMenuName");
+            _.Property<string>(x => x.NavMenuTitle).HasColumnName("NavMenuTitle");
+            _.Property<string>(x => x.NavMenuRoute).HasColumnName("NavMenuRoute");
+        }
         #endregion
 
         #region SECURITY
@@ -43,6 +56,14 @@ namespace Pizza.Express.Context
             _.Property<int>(x => x.UserId).HasColumnName("UserId");
             _.Property<int>(x => x.UserRoleId).HasColumnName("FK_UserRoleId");
             _.Property<string>(x => x.UserName).HasColumnName("UserName");
+        }
+        private void DBNavMenuRole(EntityTypeBuilder<NavMenuRoleEntity> _)
+        {
+            _.ToTable("NavMenuRole", "security");
+            _.HasKey(x => x.NavMenuRoleId);
+            _.Property<int>(x => x.NavMenuRoleId).HasColumnName("NavMenuRoleId");
+            _.Property<int>(x => x.NavMenuId).HasColumnName("FK_NavMenuId");
+            _.Property<int>(x => x.UserRoleId).HasColumnName("FK_UserRoleId");
         }
         #endregion
     }
