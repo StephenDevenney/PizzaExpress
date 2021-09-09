0d95ef16843ae8c50a7444010d2fa65a40991042
Stephen <33627847+StephenDevenney@users.noreply.github.com>
2021-09-09 21:57:24 +0100

    basket handler, get put post delete

---
+++ PizzaBackend/Pizza.Express.Handler/BasketHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Pizza.Express.Context.Interfaces;
+using Pizza.Express.Handler.Interfaces;
+using Pizza.Express.Shared.ViewModels;
+
+namespace Pizza.Express.Handler
+{
+    public class BasketHandler : IBasketHandler
+    {
+        #region CONSTRUCTOR
+        private readonly IBasketContext basketRepo;
+        private readonly IGlobals globals;
+        public BasketHandler(IGlobals _globals, 
+                                IBasketContext basketRepo)
+        {
+            this.basketRepo = basketRepo;
+            this.globals = _globals;
+        }
+        #endregion
+
+        #region GET
+        public async Task<List<BasketItemViewModel>> GetBasket() => await basketRepo.GetBasket();
+        #endregion
+
+        #region PUT
+        public async Task<List<BasketItemViewModel>> UpdateBasketItem(BasketItemViewModel basketItem) => await basketRepo.UpdateBasketItem(basketItem);
+        #endregion
+
+        #region POST
+        public async Task<List<BasketItemViewModel>> AddItemToBasket(BasketItemViewModel basketItem) => await basketRepo.AddItemToBasket(basketItem);
+        #endregion
+
+        #region DELETE
+        public async Task DeleteItemFromBasket(BasketItemViewModel basketItem) => await basketRepo.DeleteItemFromBasket(basketItem);
+        #endregion
+    }
+}
---
+++ PizzaBackend/Pizza.Express.Handler/Interfaces/IBasketHandler.cs
@@ -0,0 +1,25 @@
+﻿using Pizza.Express.Shared.ViewModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Pizza.Express.Handler.Interfaces
+{
+    public interface IBasketHandler
+    {
+        #region GET
+        public Task<List<BasketItemViewModel>> GetBasket();
+        #endregion
+
+        #region PUT
+        public Task<List<BasketItemViewModel>> UpdateBasketItem(BasketItemViewModel basketItem);
+        #endregion
+
+        #region POST
+        public Task<List<BasketItemViewModel>> AddItemToBasket(BasketItemViewModel basketItem);
+        #endregion
+
+        #region DELETE
+        public Task DeleteItemFromBasket(BasketItemViewModel basketItem);
+        #endregion
+    }
+}
