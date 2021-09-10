cce5c7279b5aa89ac8f78a2ad04f0d7dfe342cfe
Stephen <33627847+StephenDevenney@users.noreply.github.com>
2021-09-10 11:07:33 +0100

    order handler

---
+++ PizzaBackend/Pizza.Express.Handler/Interfaces/IOrderHandler.cs
@@ -0,0 +1,21 @@
+﻿using Pizza.Express.Shared.ViewModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Pizza.Express.Handler.Interfaces
+{
+    public interface IOrderHandler
+    {
+        #region GET
+        public Task<List<OrderViewModel>> GetOrderList();
+        #endregion
+
+        #region PUT
+        public Task UpdateOrderStatus(OrderViewModel order);
+        #endregion
+
+        #region POST
+        public Task CreateOrder(List<BasketItemViewModel> basket);
+        #endregion
+    }
+}
---
+++ PizzaBackend/Pizza.Express.Handler/OrderHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Pizza.Express.Context.Interfaces;
+using Pizza.Express.Handler.Interfaces;
+using Pizza.Express.Shared.ViewModels;
+
+namespace Pizza.Express.Handler
+{
+    public class OrderHandler : IOrderHandler
+    {
+        #region CONSTRUCTOR
+        private readonly IOrderContext orderRepo;
+        private readonly IGlobals globals;
+        public OrderHandler(IGlobals _globals, 
+                                IOrderContext orderRepo)
+        {
+            this.orderRepo = orderRepo;
+            this.globals = _globals;
+        }
+        #endregion
+
+        #region GET
+        public async Task<List<OrderViewModel>> GetOrderList() => await orderRepo.GetOrderList();
+        #endregion
+
+        #region PUT
+        public async Task UpdateOrderStatus(OrderViewModel order) => await orderRepo.UpdateOrderStatus(order);
+        #endregion
+
+        #region POST
+        public async Task CreateOrder(List<BasketItemViewModel> basket) => await orderRepo.CreateOrder(basket);
+        #endregion
+    }
+}
